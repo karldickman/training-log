dd5553b91755332da28547b1f5140eedebaeb09d
karldickman <karldickman@gmail.com>
2014-09-07 16:47:26 -0700

    Reconfigured bike log to allow user to specify routes instead of distances.

--- bike_log.py
+++ bike_log.py
@@ -1,66 +1,198 @@
 #!/usr/bin/python
 """Your docstring here."""
 
-from datetime import date
+from datetime import date as Date
+from flotrack_upload import FlotrackConnection
+from getpass import getpass as prompt_for_password
+from math import floor
 from miscellaneous import main_function
 from MySQLdb import connect as MySqlConnection
-from optparse import OptionParser
+from optparse import OptionParser as BaseOptionParser
 
-def log_ride(ride_date, route, distance, time, bike_id):
-    route = route.replace("\\", "\\\\").replace("\"", "\\\"")
-    connection = MySqlConnection("localhost", "bike_log", "bike_log", "bike_log")
-    cursor = connection.cursor()
+def log_ride(ride_date, bike_id, route, time_minutes, distance_miles):
+    """Log a ride to the database and Flotrack using the specified parameters."""
+    flotrack_password = prompt_for_password("Flotrack password: ")
+    with FlotrackConnection("karldickman", flotrack_password) as flotrack, \
+            new_connection() as database:
+        log_ride_to_database(database, ride_date, bike_id, route, time_minutes,
+                             distance_miles)
+        log_ride_to_flotrack(flotrack, ride_date, bike_id, route, time_minutes,
+                             distance_miles)
+
+def log_ride_to_database(database, ride_date, bike_id, route, time_minutes, distance_miles):
+    """Log a ride to the database using the specified parameters."""
     query = """INSERT INTO rides
-        (ride_date, ride, distance_miles, time_minutes, bicycle_id)
+        (ride_date, time_minutes, bicycle_id)
         VALUES
-        ("%s", "%s", %lf, %lf, %d)"""
-    query %= (ride_date.isoformat(), route, distance, time, bike_id)
-    cursor.execute(query)
-    connection.commit()
-    connection.close()
+        (%s, %s, %s)"""
+    database.execute(query, (ride_date, time_minutes, bike_id))
+    query = "SELECT LAST_INSERT_ID()"
+    database.execute(query)
+    ride_id = database.fetchone()[0]
+    if route.route_id is not None:
+        query = """INSERT INTO ride_routes
+            (ride_id, route_id)
+            VALUES
+            (%s, %s)"""
+        database.execute(query, (ride_id, route.route_id))
+    else:
+        query = """INSERT INTO ride_descriptions
+            (ride_id, ride_description)
+            VALUES
+            (%s, %s)"""
+        database.execute(query, (ride_id, route.description))
+    if distance_miles is not None:
+        query = """INSERT INTO ride_distances
+            (ride_id, distance_miles)
+            VALUES
+            (%s, %s)"""
+        database.execute(query, (ride_id, distance_miles))
+
+def log_ride_to_flotrack(flotrack, ride_date, bike_id, route, time_minutes,
+                         distance_miles):
+    """Log a ride to flotrack."""
+    run_distance_miles = run_to_bike(distance_miles, time_minutes)
+    run_time_minutes = run_distance_miles * 7.5
+    minutes_component = int(floor(time_minutes))
+    seconds_component = 60 * (time_minutes - minutes_component)
+    notes = "Route: %s\nMiles: %.2lf\nMinutes: %d:%05.2lf"
+    notes %= (route, distance_miles, minutes_component, seconds_component)
+    success = flotrack.record_run(ride_date, "Commute (Bike)",
+                                    run_distance_miles, run_time_minutes,
+                                    notes)
+    if not success:
+        raise Exception("Could not write bike ride as run to Flotrack.")
+
+def new_connection():
+    """Open a new connection to the database."""
+    database = MySqlConnection("localhost", "bike_log", "bike_log", "bike_log")
+    database.autocommit(False)
+    return database
 
 def parse_arguments(arguments):
-    usage_string = "%prog <route> <miles> <hh>:<mm> <bike_id> [--date=<yyyy-mm-dd>]"
-    option_parser = OptionParser(usage = usage_string)
-    option_parser.add_option("-d", "--date", help = "The date on which to log the ride.")
-    #And so on
-    options, arguments = option_parser.parse_args(arguments[1:])
-    if len(arguments) != 4:
-        option_parser.error("Incorrect number of arguments.")
-    route, distance_string, time_string, bike_id_string = arguments
-    # Parse distance
-    try:
-        distance = float(distance_string)
-    except:
-        option_parser.error("Improperly formatted distance.")
-    # Parse time
-    try:
-        time = 0.0
-        for minute_second_string in time_string.split("+"):
-            minutes_string, seconds_string = minute_second_string.split(":")
-            time += float(minutes_string) + float(seconds_string) / 60.0
-    except:
-        option_parser.error("Improperly formatted time.")
-    # Parse bike ID
-    try:
-        bike_id = int(bike_id_string)
-    except:
-        option_parser.error("Improperly formatted bicycle ID.")
-    # DONE!
-    arguments = route, distance, time, bike_id
-    if options.date is None:
-        options.date = date.today()
-    else:
+    """Parse the command-line arguments."""
+    option_parser = OptionParser()
+    return option_parser.parse_args(arguments)
+
+def run_to_bike(distance_miles, time_minutes):
+    """Convert a bike ride to running miles."""
+    time_hours = time_minutes / 60.0
+    speed_mph = distance_miles / time_hours
+    return distance_miles * (0.0003736 * speed_mph ** 2 + 0.1973)
+
+class OptionParser(BaseOptionParser):
+    """Option parser for this command-line utility."""
+    def __init__(self):
+        usage_string = "%prog <route> <bike_id> <hh:mm>+[<hh:mm>]\
+                [<miles>[+<more_miles>]] [<yyyy-mm-dd>]"
+        BaseOptionParser.__init__(self, usage=usage_string)
+        self.add_option("--flotrack", default=False, action="store_true",
+                        help="Upload ride to flotrack")
+
+    def parse_args(self, arguments):
+        options, arguments = BaseOptionParser.parse_args(self, arguments[1:])
+        if len(arguments) < 3:
+            self.error("Incorrect number of arguments.")
+        route_string = arguments[0]
+        bike_string = arguments[1]
+        time_string = arguments[2]
+        options.route = self.parse_route(route_string)
+        options.bike_id = self.parse_bicycle(bike_string)
+        options.time_minutes = self.parse_time(time_string)
+        if not hasattr(options, "distance_miles"):
+            if len(arguments) > 3:
+                distance_string = arguments[3]
+                options.distance_miles = self.parse_distance(distance_string)
+            elif options.route.route_id is None:
+                self.error("Ride distance not specified.")
+            else:
+                options.distance_miles = None
+        # Parse date
+        if len(arguments) == 5 \
+                or options.distance_miles is None and len(arguments) == 4:
+            date_string = arguments[-1]
+            try:
+                year_string, month_string, day_string = date_string.split("-")
+                ride_date = Date(int(year_string), int(month_string), \
+                                 int(day_string))
+            except ValueError:
+                self.error("Improperly formatted date.")
+        else:
+            ride_date = Date.today()
+        options.date = ride_date
+        return options, []
+
+    def parse_bicycle(self, bike_string):
+        """Convert a bicycle string into a bicycle id."""
+        with new_connection() as database:
+            query = "SELECT bicycle_id FROM bicycle_names WHERE bicycle = %s"
+            if database.execute(query, bike_string) > 0:
+                return database.fetchone()[0]
+            try:
+                return int(bike_string)
+            except ValueError:
+                self.error("Improperly formatted bicycle ID.")
+
+    def parse_distance(self, distance_string):
+        """Convert a distance string into a decimal distance."""
         try:
-            year_string, month_string, day_string = options.date.split("-")
-            options.date = date(int(year_string), int(month_string), int(day_string))
-        except:
-            option_parser.error("Improperly formatted date.")
-    return options, arguments
+            distance = 0.0
+            for segment_distance_string in distance_string.split("+"):
+                distance += float(segment_distance_string)
+        except ValueError:
+            self.error("Improperly formatted distance.")
+        return distance
+
+    def parse_route(self, route_string):
+        """Convert a route string into route information."""
+        with new_connection() as database:
+            query = "SELECT route_id FROM routes WHERE route = %s"
+            if database.execute(query, route_string) > 0:
+                return Route(route_id=database.fetchone()[0])
+            else:
+                return Route(description=route_string)
+
+    def parse_time(self, time_string):
+        """Convert a time string into a decimal time."""
+        try:
+            time = 0.0
+            for minute_second_string in time_string.split("+"):
+                split_by_colon = minute_second_string.split(":")
+                if len(split_by_colon) > 2:
+                    self.error("Improperly formatted time.")
+                minutes_string = split_by_colon[0]
+                if len(split_by_colon) > 1:
+                    seconds_string = split_by_colon[1]
+                else:
+                    seconds_string = 0
+                time += float(minutes_string) + float(seconds_string) / 60.0
+        except ValueError:
+            self.error("Improperly formatted time.")
+        return time
+
+class Route(object):
+    """Represents information about the route that was taken."""
+
+    def __init__(self, route_id=None, description=None):
+        self.route_id = route_id
+        self.description = description
+
+    def __repr__(self):
+        return "bike_log.Route(%d, %s)" % \
+                (self.route_id, repr(self.description))
 
 @main_function(parse_arguments)
 def main(options, arguments):
-    log_ride(options.date, *arguments)
+    """Process command line arguments and use them to write to the log."""
+    options.flotrack = False
+    if options.flotrack:
+        log_ride(options.date, options.bike_id, options.route,
+                options.time_minutes, options.distance_miles)
+    else:
+        with new_connection() as database:
+            log_ride_to_database(database, options.date, options.bike_id,
+                                 options.route, options.time_minutes,
+                                 options.distance_miles)
 
 if __name__ == "__main__":
     main()
--- flotrack_upload.py
+++ flotrack_upload.py
@@ -1,36 +1,229 @@
 #!/usr/bin/python
 """Upload a running log to Flotrack."""
 
+from cookielib import MozillaCookieJar
+from datetime import date as Date
+from getpass import getpass as prompt_for_password
 from httplib import HTTPConnection as HttpConnection
+from httplib import FOUND
+from httplib import OK
+from math import floor
 from miscellaneous import main_function
+from optparse import OptionParser
+from os import path
+from urllib import urlencode as url_encode
+
+class FlotrackConnection(object):
+    """Provides an interface for connecting to the Flotrack website
+    and logging runs."""
+
+    # The domain name of the flotrack website
+    flotrack_domain = "www.flotrack.org"
+    # The URL to use to log in
+    login_url = "/site/login"
+    # The URL to use to log out
+    logout_url = "/site/logout"
+    # The URL to use to record runs
+    running_log_url = "/running_logs/new/%s"
+    # The file containing the cookies
+    cookie_file = "/home/karl/.flotrack-cookies"
+
+    def __enter__(self):
+        self.cookie_jar.load()
+        if not self.login(self.username, self.password):
+            raise Exception("Invalid flotrack password.")
+        return self
+
+    def __exit__(self, type_, value, tb):
+        try:
+            self.cookie_jar.save()
+            self.connection.close()
+        except:
+            pass
+
+    def __init__(self, username, password, debug_level=0):
+        self.username = username
+        self.password = password
+        if not path.exists(self.cookie_file):
+            with open(self.cookie_file, "w+"):
+                pass
+        self.cookie_jar = MozillaCookieJar(self.cookie_file)
+        self.debug_level = debug_level
+
+    def get_default_headers(self, content, content_type=None):
+        content_type = "application/x-www-form-urlencoded"
+        cookies = "; ".join(("%s=%s" % (c.name, c.value) for c in self.cookie_jar))
+        return { "Accept": "text/html,application/xhtml+xml",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "en-US,en",
+                "Content-Length": len(content),
+                "Content-Type": content_type,
+                "Connection": "keep-alive",
+                "Cookie": cookies, }
+
+    def get_running_log_params(self, date, route, distance_miles, time_minutes, notes):
+        minutes_component = floor(time_minutes)
+        seconds_component = (time_minutes - minutes_component) * 60
+        date_string = date.isoformat()
+        run_info = [("date", date_string),
+                    ("log_type", ""),
+                    ("log_type", "run"),
+                    ("crossTrainType", ""),
+                    ("workout", ""),
+                    ("workout", ""),
+                    ("title", route),
+                    ("show_on_menu", 0),
+                    ("date", date_string),
+                    ("warmup_mins", ""),
+                    ("warmup_secs", ""),
+                    ("warmup_distance", ""),
+                    ("warmup_dist_unit", "miles"),
+                    ("warmup_shoe_id", ""),
+                    ("add_log", ""),
+                    ("interval[0][reps]", ""),
+                    ("interval[0][distance]", ""),
+                    ("interval[0][dist_unit]", "miles"),
+                    ("interval[0][rest]", ""),
+                    ("interval[0][rest_unit]", "secs"),
+                    ("interval[0][calculate_pace]", 1),
+                    ("interval[0][shoe_id]", ""),
+                    ("cooldown_mins", ""),
+                    ("cooldown_secs", ""),
+                    ("cooldown_distance", ""),
+                    ("cooldown_dist_unit", "miles"),
+                    ("cooldown_shoe_id", ""),
+                    ("mins", int(minutes_component)),
+                    ("secs", int(seconds_component)),
+                    ("distance", distance_miles),
+                    ("dist_unit", "miles"),
+                    ("calculate_pace", 0),
+                    ("calculate_pace", 1),
+                    ("feel", ""),
+                    ("field1", ""),
+                    ("field2", ""),
+                    ("shoe_id", ""),
+                    ("notes", notes),
+                    ("add_log", ""),]
+        for i in range(len(run_info)):
+            key = run_info[i][0]
+            value = run_info[i][1]
+            if key != "add_log" and "interval[0]" not in key:
+                key = "RunningLogResource[%s]" % key
+                run_info[i] = (key, value)
+        return url_encode(run_info)
+
+    def get_running_log_url(self, date):
+        date_string = format(date, "%Y/%m/%d")
+        return self.running_log_url % date_string
+
+    def login(self, username, password):
+        connection = HttpConnection(self.flotrack_domain)
+        connection.set_debuglevel(self.debug_level)
+        connection.connect()
+        # Configure login parameters (this is the content of the HTTP request)
+        params = { "LoginForm[username]": username,
+                   "LoginForm[password]": password,
+                   "LoginForm[rememberMe]": 1, }
+        encoded_params = url_encode(params)
+        # Get the HTTP headers to use
+        headers = self.get_default_headers(encoded_params)
+        del headers["Cookie"]
+        # Configure the cookie jar to store the login information
+        cookie_request = HttpRequestAdapter(self.flotrack_domain, self.login_url,
+                                            headers)
+        self.cookie_jar.add_cookie_header(cookie_request)
+        # Issue the HTTP request
+        request = connection.request("POST", self.login_url, encoded_params, headers)
+        response = connection.getresponse()
+        if response.status == OK:
+            return False
+        if response.status == FOUND:
+            response.read()
+            response.info = lambda: response.msg
+            # Extract the login cookies
+            self.cookie_jar.extract_cookies(response, cookie_request)
+            self.connection = connection
+            return True
+        raise Exception("Flotrack connection failed during login.")
+
+    def logout(self):
+        request = self.connection.request("GET", self.logout_url)
+        response = self.connection.getresponse()
+        if response.status == OK:
+            return False
+        if response.status == FOUND:
+            return True
+        raise Exception("Flotrack connection failed during logout.")
+
+    def record_run(self, date, route, distance_miles, time_minutes, notes=""):
+        # Create parameters to pass to the log
+        encoded_params = self.get_running_log_params(date, route, distance_miles, time_minutes, notes)
+        # Post the data to the server
+        headers = self.get_default_headers(encoded_params)
+        running_log_url = self.get_running_log_url(date)
+        request = self.connection.request("POST", running_log_url, encoded_params, headers)
+        response = self.connection.getresponse()
+        if response.status == OK:
+            return False
+        if response.status == FOUND:
+            response.read()
+            return True
+        raise Exception("Flotrack connection failed while recording run.")
+
+class HttpRequestAdapter(object):
+    """Data container for HTTP request (used for cookie processing)."""
+
+    def __init__(self, host, url, headers={}):
+        self._host = host
+        self._url = url
+        self._headers = {}
+        for key, value in headers.items():
+            self.add_header(key, value)
+
+    def has_header(self, name):
+        return name in self._headers
+
+    def add_header(self, key, val):
+        self._headers[key.capitalize()] = val
+
+    def add_unredirected_header(self, key, val):
+        self._headers[key.capitalize()] = val
+
+    def is_unverifiable(self):
+        return True
+
+    def get_type(self):
+        return "http"
+
+    def get_full_url(self):
+        # TODO: implement other protocols support
+        return "http://" + self._host[0] + ":" + str(self._host[1]) + self._url
+
+    def get_header(self, header_name, default=None):
+        return self._headers.get( header_name, default )
+
+    def get_host(self):
+        return self._host[0]
+
+    get_origin_req_host = get_host
+
+    def get_headers(self):
+        return self._headers
 
 def parse_arguments(arguments):
-    usage = ""
+    usage = "%prog USER"
     option_parser = OptionParser(usage)
-    #And so on
-    return option_parser.parse_args(arguments[1:])
+    options, arguments = option_parser.parse_args(arguments[1:])
+    if len(arguments) != 1:
+        option_parser.error("Incorrect number of arguments.")
+    options.username = arguments[0]
+    return options, []
 
-#@main_function(parse_arguments)
-def main():#options, arguments):
-    flotrack_domain = "www.flotrack.org"
-    request_url = "/running_logs/day/2013/04/01/user/karldickman"
-    connection = HttpConnection(flotrack_domain)
-    connection.set_debuglevel(1)
-    connection.connect()
-    request = connection.putrequest("POST", request_url)
-    headers = { "Content-Type": "application/x-www-form-urlencoded",
-               "User-Agent": "Dickman's Flotrack Upload Client",
-               "Accept": "*/*", }
-    for key, value in headers.iteritems():
-        connection.putheader(key, value)
-    connection.endheaders()
-    data = "RunningLogResource%5Bdate%5D=2013-04-01&RunningLogResource%5Blog_type%5D=&RunningLogResource%5Blog_type%5D=run&RunningLogResource%5BcrossTrainType%5D=&RunningLogResource%5Bworkout%5D=&RunningLogResource%5Bworkout%5D=&RunningLogResource%5Btitle%5D=Test&RunningLogResource%5Bshow_on_menu%5D=0&RunningLogResource%5Bdate%5D=2013-04-01&RunningLogResource%5Bwarmup_mins%5D=&RunningLogResource%5Bwarmup_secs%5D=&RunningLogResource%5Bwarmup_distance%5D=&RunningLogResource%5Bwarmup_dist_unit%5D=miles&RunningLogResource%5Bwarmup_shoe_id%5D=&interval%5B0%5D%5Breps%5D=&interval%5B0%5D%5Bdistance%5D=&interval%5B0%5D%5Bdist_unit%5D=miles&interval%5B0%5D%5Brest%5D=&interval%5B0%5D%5Brest_unit%5D=secs&interval%5B0%5D%5Bcalculate_pace%5D=1&interval%5B0%5D%5Bshoe_id%5D=&RunningLogResource%5Bcooldown_mins%5D=&RunningLogResource%5Bcooldown_secs%5D=&RunningLogResource%5Bcooldown_distance%5D=&RunningLogResource%5Bcooldown_dist_unit%5D=miles&RunningLogResource%5Bcooldown_shoe_id%5D=&RunningLogResource%5Bmins%5D=3&RunningLogResource%5Bsecs%5D=14&RunningLogResource%5Bdistance%5D=3.14159&RunningLogResource%5Bdist_unit%5D=miles&RunningLogResource%5Bcalculate_pace%5D=0&RunningLogResource%5Bcalculate_pace%5D=1&RunningLogResource%5Bfeel%5D=&RunningLogResource%5Bfeel%5D=3&RunningLogResource%5Bfield1%5D=&RunningLogResource%5Bfield2%5D=&RunningLogResource%5Bshoe_id%5D=&RunningLogResource%5Bnotes%5D=&add_log="
-    connection.send(data)
-    response = connection.getresponse()
-    print response.status
-    print response.reason
-    print response.read()
-    connection.close()
+@main_function(parse_arguments)
+def main(options, arguments):
+    password = prompt_for_password("Flotrack password: ")
+    with FlotrackConnection(options.username, password) as connection:
+        connection.record_run(Date.today(), "Test", 3.14159, 3 + 14.0 / 60)
 
 if __name__ == "__main__":
     main()
