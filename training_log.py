e337568b9e35b5a1539a1875472f8aaa9179ae24
karldickman <karldickman@gmail.com>
2020-06-14 13:52:03 -0700

    Run command against postgres database.

--- training_log.py
+++ training_log.py
@@ -1,56 +1,39 @@
 #!/usr/bin/python
 """Command-line tool to log training to a database."""
 
+from configparser import ConfigParser
 from datetime import date as Date
 from getpass import getpass as prompt_for_password
 from math import floor
-from miscellaneous import main_function
-from MySQLdb import connect as MySqlConnection
 from optparse import OptionParser as BaseOptionParser
+from psycopg2 import connect
+from sys import argv
 
-def log_ride_to_database(database, ride_date, bike_id, route, time_minutes,
-                         distance_miles):
-    """Log a ride to the database using the specified parameters."""
-    query = """INSERT INTO rides
-        (ride_date, bicycle_id)
-        VALUES
-        (%s, %s)"""
-    database.execute(query, (ride_date, bike_id))
-    query = "SELECT LAST_INSERT_ID()"
-    database.execute(query)
-    ride_id = database.fetchone()[0]
-    if route.route_id is not None:
-        query = """INSERT INTO ride_routes
-            (ride_id, route_id)
-            VALUES
-            (%s, %s)"""
-        database.execute(query, (ride_id, route.route_id))
+def config(filename="database.ini", section="postgresql"):
+    parser = ConfigParser()
+    parser.read(filename)
+    database = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            database[param[0]] = param[1]
     else:
-        query = """INSERT INTO ride_descriptions
-            (ride_id, ride_description)
-            VALUES
-            (%s, %s)"""
-        database.execute(query, (ride_id, route.description))
-    if time_minutes is not None:
-        query = """INSERT INTO ride_durations
-            (ride_id, duration_minutes)
-            VALUES
-            (%s, %s)"""
-        database.execute(query, (ride_id, time_minutes))
-    if distance_miles is not None:
-        query = """INSERT INTO ride_non_route_distances
-            (ride_id, distance_miles)
-            VALUES
-            (%s, %s)"""
-        database.execute(query, (ride_id, distance_miles))
-    return ride_id
+        raise Exception(f"Section{section} not found in the {filename} file.")
+    return database
+
+def record_activity(cursor, activity_date, activity_type_id, equipment_id,
+        route, duration_minutes, distance_miles):
+    """Record an activity in the database using the specified parameters."""
+    params = (activity_date, activity_type_id, equipment_id, route.route_id,
+        route.description, duration_minutes, distance_miles)
+    cursor.callproc("record_activity", params)
+    (activity_id,) = cursor.fetchone()
+    return activity_id
 
 def new_connection():
     """Open a new connection to the database."""
-    database = MySqlConnection("localhost", "training_log", "training_log",
-                               "pGJDwnnu5EqXHEKHfWU7")
-    database.autocommit(False)
-    return database
+    params = config()
+    return connect(**params)
 
 def parse_arguments(arguments):
     """Parse the command-line arguments."""
@@ -59,32 +42,33 @@ def parse_arguments(arguments):
 
 def parse_route(route_string):
     """Convert a route string into route information."""
-    with new_connection() as database:
-        query = "SELECT route_id FROM routes WHERE route = %s"
-        if database.execute(query, [route_string]) > 0:
-            return Route(route_id=database.fetchone()[0])
-        return Route(description=route_string)
+    with new_connection() as database, database.cursor() as cursor:
+        cursor.callproc("get_route_id_by_name", (route_string,))
+        (route_id,) = cursor.fetchone()
+        if route_id is None:
+            return Route(description=route_string)
+        return Route(route_id=route_id)
 
-def run_to_bike(distance_miles, time_minutes):
+def run_to_bike(distance_miles, duration_minutes):
     """Convert a bike ride to running miles."""
-    time_hours = time_minutes / 60.0
-    speed_mph = distance_miles / time_hours
+    duration_hours = duration_minutes / 60.0
+    speed_mph = distance_miles / duration_hours
     return distance_miles * (0.0003736 * speed_mph ** 2 + 0.1973)
 
 class OptionParser(BaseOptionParser):
     """Option parser for this command-line utility."""
     def __init__(self):
-        usage_string = "%prog <activity> <route> [<time hh:mm:ss>] [<distance miles>] [<yyyy-mm-dd>]"
+        usage_string = "%prog <activity> <route> [<duration hh:mm:ss>] [<distance miles>] [<yyyy-mm-dd>]"
         BaseOptionParser.__init__(self, usage=usage_string)
-        self.add_option("-q", "--equipment", default=None,
+        self.add_option("--equipment", default=None,
                         help="The equipment used for the session.")
-        self.add_option("-d", "--date", default=None,
+        self.add_option("--date", default=None,
                         help="The date on which the session occurred.")
-        self.add_option("-m", "--distance-miles", default=None,
+        self.add_option("--distance-miles", default=None,
                         help="The length of the session in miles.")
         self.add_option("--print-id", default=False, action="store_true",
                         help="Print the database ID of the recorded session.")
-        self.add_option("-t", "--time", default=None,
+        self.add_option("--duration", default=None,
                         help="The duration of the session.",
                         metavar="[HH:]MM:SS")
 
@@ -92,15 +76,14 @@ class OptionParser(BaseOptionParser):
         options, arguments = BaseOptionParser.parse_args(self, arguments[1:])
         if len(arguments) < 2 or len(arguments) > 5:
             self.error("Incorrect number of arguments.")
+        arguments.pop(0)
         route_string = arguments.pop(0)
-        bike_string = arguments.pop(0)
         options.route = parse_route(route_string)
-        options.bike_id = self.parse_bicycle(bike_string)
         if len(arguments) > 0:
-            time_string = arguments.pop(0)
-            options.time_minutes = self.parse_time(time_string)
+            duration_string = arguments.pop(0)
+            options.duration_minutes = self.parse_duration(duration_string)
         else:
-            options.time_minutes = None
+            options.duration_minutes = None
         if options.route.route_id is None or len(arguments) > 0:
             if any(arguments):
                 distance_string = arguments.pop(0)
@@ -135,18 +118,22 @@ class OptionParser(BaseOptionParser):
                 self.error("Improperly formatted date.")
         elif options.date is None:
             options.date = Date.today()
-        return options, []
+        options.equipment_id = self.parse_equipment(options.equipment)
+        return options, arguments
 
-    def parse_bicycle(self, bike_string):
-        """Convert a bicycle string into a bicycle id."""
-        with new_connection() as database:
-            query = "SELECT bicycle_id FROM bicycle_names WHERE bicycle = %s"
-            if database.execute(query, [bike_string]) > 0:
-                return database.fetchone()[0]
+    def parse_equipment(self, equipment_string):
+        """Convert an equipment string into an equipment id."""
+        if equipment_string is None:
+            return None
+        with new_connection() as database, database.cursor() as cursor:
+            cursor.callproc("get_equipment_id_by_label", (equipment_string,))
+            (equipment_id,) = cursor.fetchone()
+            if equipment_id is not None:
+                return equipment_id
             try:
-                return int(bike_string)
+                return int(equipment_string)
             except ValueError:
-                self.error("Improperly formatted bicycle ID.")
+                self.error("Improperly formatted equipment.")
 
     def parse_distance(self, distance_string):
         """Convert a distance string into a decimal distance."""
@@ -159,14 +146,14 @@ class OptionParser(BaseOptionParser):
         return distance
 
 
-    def parse_time(self, time_string):
-        """Convert a time string into a decimal time."""
+    def parse_duration(self, duration_string):
+        """Convert a duration string into a decimal duration."""
         try:
-            time = 0.0
-            for minute_second_string in time_string.split("+"):
+            duration = 0.0
+            for minute_second_string in duration_string.split("+"):
                 split_by_colon = minute_second_string.split(":")
                 if len(split_by_colon) > 3:
-                    self.error("Improperly formatted time.")
+                    self.error("Improperly formatted duration.")
                 seconds = float(split_by_colon.pop())
                 if split_by_colon:
                     minutes = float(split_by_colon.pop())
@@ -176,10 +163,10 @@ class OptionParser(BaseOptionParser):
                     hours = float(split_by_colon.pop())
                 else:
                     hours = 0
-                time += hours * 60.0 + minutes + seconds / 60.0
+                duration += hours * 60.0 + minutes + seconds / 60.0
         except ValueError:
-            self.error("Improperly formatted time.")
-        return time
+            self.error("Improperly formatted duration.")
+        return duration
 
 class Route(object):
     """Represents information about the route that was taken."""
@@ -194,15 +181,15 @@ class Route(object):
         return "bike_log.Route(%d, %s)" % \
                 (self.route_id, repr(self.description))
 
-@main_function(parse_arguments)
-def main(options, arguments):
+def main():
     """Process command line arguments and use them to write to the log."""
-    with new_connection() as database:
-        ride_id = log_ride_to_database(database, options.date, options.bike_id,
-                                       options.route, options.time_minutes,
-                                       options.distance_miles)
+    option_parser = OptionParser()
+    options, _ = option_parser.parse_args(argv)
+    with new_connection() as database, database.cursor() as cursor:
+        ride_id = record_activity(cursor, options.date, 1, options.equipment_id, options.route,
+                options.duration_minutes, options.distance_miles)
         if options.print_id:
-            print ride_id
+            print(ride_id)
 
 if __name__ == "__main__":
     main()
