cf5b11f69da6c6a92a80954587cc0be8f18e39e9
karldickman <karldickman@gmail.com>
2020-06-14 12:38:46 -0700

    Converted from stored procedure to stored function, renamed variable.

--- sql/procedures/record_activity.sql
+++ sql/procedures/record_activity.sql
@@ -1,20 +1,21 @@
-CREATE OR REPLACE PROCEDURE record_activity (
+CREATE OR REPLACE FUNCTION record_activity (
 	activity_date date,
 	activity_type_id integer,
 	equipment_id integer DEFAULT NULL,
 	route_id integer DEFAULT NULL,
 	route_description character varying DEFAULT NULL,
 	duration_minutes double precision DEFAULT NULL,
-	distance_mi double precision DEFAULT NULL,
-	INOUT activity_id integer DEFAULT NULL)
-LANGUAGE plpgsql
+	distance_miles double precision DEFAULT NULL)
+RETURNS integer
 AS $$
+DECLARE activity_id integer;
 BEGIN
 	INSERT INTO activities
 		(activity_date, activity_type_id)
 		VALUES
 		(activity_date, activity_type_id)
-		RETURNING activity_id;
+		RETURNING activities.activity_id
+		INTO activity_id;
 	IF route_id IS NOT NULL THEN
 		INSERT INTO activity_routes
 			(activity_id, route_id)
@@ -36,9 +37,10 @@ BEGIN
 		INSERT INTO activity_non_route_distances
 			(activity_id, distance_miles)
 			VALUES
-			(activity_id, distance_mi);
+			(activity_id, distance_miles);
 	END IF;
+	RETURN(activity_id);
 END;
-$$;
+$$ LANGUAGE plpgsql;
 
-ALTER PROCEDURE record_activity OWNER TO postgres;
+ALTER FUNCTION record_activity OWNER TO postgres;
