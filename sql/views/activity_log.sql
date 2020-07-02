4266ae82466b30cff5e878919f9c3fd056a44cf5
karldickman <karldickman@gmail.com>
2020-07-02 09:42:03 -0700

    Added pace column to activity log view.

--- sql/views/activity_log.sql
+++ sql/views/activity_log.sql
@@ -1,19 +1,26 @@
+-- DROP VIEW activity_log;
 CREATE OR REPLACE VIEW activity_log
 AS
 SELECT activity_id
         , activities.activity_date AS date
         , activity_description AS description
         , activity_type AS type
-        , duration_minutes
-        , distance_miles
+        , activity_durations.duration_minutes
+        , activity_distances.distance_miles
+        , CASE
+            WHEN activity_type = 'bike'
+                THEN TO_CHAR(speed_miles_per_hour, '99.9') || ' mph'
+            ELSE TO_CHAR((pace_minutes_per_mile || ' minute')::INTERVAL, 'MI:SS')
+            END AS pace
         , equipment_label AS equipment
     FROM activities
     JOIN activity_types USING (activity_type_id)
     LEFT JOIN activities_labelled USING (activity_id)
     LEFT JOIN activity_durations USING (activity_id)
     LEFT JOIN activity_distances USING (activity_id)
+    LEFT JOIN activity_paces USING (activity_id)
     LEFT JOIN activity_equipment USING (activity_id)
     LEFT JOIN equipment_labelled USING (equipment_id)
-    ORDER BY activity_date DESC, activities.activity_id DESC;
+    ORDER BY activities.activity_date DESC, activities.activity_id DESC;
 
 ALTER TABLE activity_log OWNER TO postgres;
---
+++ sql/views/activity_paces.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW activity_paces
+AS
+SELECT activity_id
+        , activity_date
+        , distance_miles
+        , duration_minutes
+        , duration_minutes / distance_miles AS pace_minutes_per_mile
+        , distance_miles / duration_minutes * 60 AS speed_miles_per_hour
+    FROM activity_distances
+    JOIN activity_durations USING (activity_id);
+
+ALTER VIEW activity_paces OWNER TO postgres;
