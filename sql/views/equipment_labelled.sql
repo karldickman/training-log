bfcc07cffa186221a42084c2862ff6293b50630f
karldickman <karldickman@gmail.com>
2020-07-04 08:15:51 -0700

    Decompose equipment table.

--- sql/views/equipment_labelled.sql
+++ sql/views/equipment_labelled.sql
@@ -6,11 +6,16 @@ SELECT equipment_id
      , cost
      , purchase_date
      , is_active
-     , is_active_since
+     , CASE
+        WHEN is_active
+            THEN purchase_date
+        ELSE inactive_date
+        END AS is_active_since
     FROM equipment
     LEFT JOIN equipment_labels USING (equipment_id)
     LEFT JOIN equipment_costs USING (equipment_id)
     LEFT JOIN equipment_model_assignments USING (equipment_id)
-    LEFT JOIN equipment_models USING (equipment_model_id);
+    LEFT JOIN equipment_models USING (equipment_model_id)
+    LEFT JOIN equipment_inactive_dates USING (equipment_id);
 
 ALTER VIEW equipment_labelled OWNER TO postgres;
