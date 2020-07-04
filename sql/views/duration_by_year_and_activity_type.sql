367045b6531450efb4737658baf576fe4f2a46be
karldickman <karldickman@gmail.com>
2020-07-04 13:43:08 -0700

    Add summary views.

---
+++ sql/views/duration_by_year_and_activity_type.sql
@@ -0,0 +1,9 @@
+-- DROP VIEW duration_by_year_and_activity_type;
+CREATE OR REPLACE VIEW duration_by_year_and_activity_type
+AS
+SELECT DATE_PART('year', activity_date) AS year, activity_type_id, SUM(duration_minutes) / 60 AS duration_hours
+    FROM activity_durations
+    NATURAL JOIN activities
+    GROUP BY DATE_PART('year', activity_date), activity_type_id;
+
+ALTER VIEW duration_by_year_and_activity_type OWNER TO postgres;
