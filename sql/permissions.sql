ac88f69b277372f49ddf78254ef321a5d36cbe5b
karldickman <karldickman@gmail.com>
2020-07-05 08:29:17 -0700

    Added missing permissions

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -4,6 +4,7 @@ GRANT SELECT, INSERT ON activity_descriptions TO workouts;
 GRANT SELECT, INSERT ON activity_durations TO workouts;
 GRANT SELECT, INSERT ON activity_equipment TO workouts;
 GRANT SELECT, INSERT ON activity_non_route_distances TO workouts;
+GRANT SELECT, INSERT ON activity_notes TO workouts;
 GRANT SELECT, INSERT ON activity_routes TO workouts;
 GRANT SELECT ON equipment_labelled TO workouts;
 GRANT SELECT ON routes TO workouts;
