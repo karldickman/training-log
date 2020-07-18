a8456475514a2b3d45e915319ef9b24cc57ba5a3
karldickman <karldickman@gmail.com>
2020-07-17 17:12:33 -0700

    Modify crosstab source query to ensure that distances are 0; previous code had bugs.

--- sql/views/equivalent_run_distance_by_week.sql
+++ sql/views/equivalent_run_distance_by_week.sql
@@ -2,10 +2,21 @@
 CREATE OR REPLACE VIEW equivalent_run_distance_by_week
 AS
 SELECT monday, strict, "elevated heart rate", loose
-    FROM CROSSTAB('SELECT monday, distance_equivalent_id, distance_miles
+    FROM CROSSTAB('SELECT week_start
+            , distance_equivalent_id
+            , distance_miles
         FROM equivalent_distance_by_week
-        WHERE activity_type_id = 1 -- run
-        ORDER BY monday, distance_equivalent_id')
+        WHERE equivalent_distance_by_week.activity_type_id = 1 -- run
+    UNION
+    SELECT monday AS week_start
+            , distance_equivalent_id
+            , 0 AS distance_miles
+        FROM week_boundaries, distance_equivalents
+        WHERE NOT EXISTS(SELECT *
+                FROM equivalent_distance_by_week
+                WHERE week_start = monday
+                    AND equivalent_distance_by_week.distance_equivalent_id = distance_equivalents.distance_equivalent_id)
+        ORDER BY week_start, distance_equivalent_id')
     AS equivalent_distance_by_week(monday DATE, strict FLOAT, "elevated heart rate" FLOAT, loose FLOAT)
     ORDER BY monday DESC;
 
