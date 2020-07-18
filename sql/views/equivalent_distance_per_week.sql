abea610532ec6be2880981a438d1cbade6c5c816
karldickman <karldickman@gmail.com>
2020-07-18 07:38:39 -0700

    Create view showing zeros where no distances were recorded.

---
+++ sql/views/equivalent_distance_per_week.sql
@@ -0,0 +1,19 @@
+-- DROP VIEW equivalent_distance_per_week;
+CREATE OR REPLACE VIEW equivalent_distance_per_week
+AS
+SELECT week_start
+        , activity_equivalence_id
+        , distance_miles
+    FROM equivalent_distance_by_week
+UNION
+SELECT monday AS week_start
+        , activity_equivalence_id
+        , 0 AS distance_miles
+    FROM week_boundaries, activity_equivalences
+    WHERE NOT EXISTS(SELECT *
+            FROM equivalent_distance_by_week
+            WHERE week_start = monday
+                AND equivalent_distance_by_week.activity_equivalence_id = activity_equivalences.activity_equivalence_id)
+    ORDER BY week_start, activity_equivalence_id;
+
+ALTER VIEW equivalent_distance_per_week OWNER TO postgres;
--- sql/views/pretty.equivalent_run_distance_by_week.sql
+++ sql/views/pretty.equivalent_run_distance_by_week.sql
@@ -5,19 +5,9 @@ SELECT monday, strict, "elevated heart rate", loose
     FROM CROSSTAB('SELECT week_start
             , activity_equivalence_id
             , ROUND(CAST(distance_miles AS NUMERIC), 1) AS distance_miles
-        FROM equivalent_distance_by_week
-        WHERE equivalent_distance_by_week.activity_type_id = 1 -- run
-    UNION
-    SELECT monday AS week_start
-            , activity_equivalence_id
-            , 0 AS distance_miles
-        FROM week_boundaries, activity_equivalences
-        WHERE NOT EXISTS(SELECT *
-                FROM equivalent_distance_by_week
-                WHERE week_start = monday
-                    AND equivalent_distance_by_week.activity_equivalence_id = activity_equivalences.activity_equivalence_id)
-        ORDER BY week_start, activity_equivalence_id')
-    AS equivalent_distance_by_week(monday DATE, strict NUMERIC, "elevated heart rate" NUMERIC, loose NUMERIC)
+        FROM equivalent_distance_per_week
+        WHERE equivalent_distance_per_week.activity_type_id = 1 -- run')
+    AS equivalent_distance_per_week(monday DATE, strict NUMERIC, "elevated heart rate" NUMERIC, loose NUMERIC)
     ORDER BY monday DESC;
 
 ALTER VIEW pretty.equivalent_run_distance_by_week OWNER TO postgres;
