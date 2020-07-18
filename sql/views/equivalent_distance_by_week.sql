0924e6feb1739b2f9ad1a51161d5b28e1dbef374
karldickman <karldickman@gmail.com>
2020-07-17 17:11:59 -0700

    Rename column.

--- sql/views/equivalent_distance_by_week.sql
+++ sql/views/equivalent_distance_by_week.sql
@@ -1,7 +1,7 @@
 -- DROP VIEW equivalent_distance_by_week;
 CREATE OR REPLACE VIEW equivalent_distance_by_week
 AS
-SELECT monday, activity_type_id, distance_equivalent_id, SUM(distance_miles) AS distance_miles
+SELECT monday AS week_start, activity_type_id, distance_equivalent_id, SUM(distance_miles) AS distance_miles
     FROM equivalent_distances_to_equivalences
     JOIN week_boundaries
         ON monday <= activity_date
