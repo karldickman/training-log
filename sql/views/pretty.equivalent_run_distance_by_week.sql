22ae39e9b97f2f7436f658b24a3db589b122d29d
karldickman <karldickman@gmail.com>
2020-07-19 17:52:56 -0700

    Fix issues in equivalent distance summary.

--- sql/views/pretty.equivalent_run_distance_by_week.sql
+++ sql/views/pretty.equivalent_run_distance_by_week.sql
@@ -6,7 +6,8 @@ SELECT monday, strict, "elevated heart rate", loose
             , activity_equivalence_id
             , ROUND(CAST(distance_miles AS NUMERIC), 1) AS distance_miles
         FROM equivalent_distance_per_week
-        WHERE equivalent_distance_per_week.activity_type_id = 1 -- run')
+        WHERE equivalent_distance_per_week.activity_type_id = 1 -- run
+        ORDER BY week_start, activity_equivalence_id')
     AS equivalent_distance_per_week(monday DATE, strict NUMERIC, "elevated heart rate" NUMERIC, loose NUMERIC)
     ORDER BY monday DESC;
 
