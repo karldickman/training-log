93a78afdfe7876dd7186a73e1f2cff2a02f075ee
karldickman <karldickman@gmail.com>
2020-08-02 06:29:03 -0700

    Show nulls on days with no activity.

--- sql/views/pretty.equivalent_run_distance_by_weekday_loose.sql
+++ sql/views/pretty.equivalent_run_distance_by_weekday_loose.sql
@@ -2,15 +2,20 @@
 CREATE OR REPLACE VIEW pretty.equivalent_run_distance_by_weekday_loose
 AS
 SELECT week_start, monday, tuesday, wednesday, thursday, friday, saturday, sunday
-    FROM CROSSTAB('SELECT monday AS week_start
+    FROM CROSSTAB('WITH equivalent_run_distance_by_day AS (SELECT activity_date
+            , activity_type_id
+            , activity_equivalence_id
+            , distance_miles
+        FROM equivalent_distance_by_day
+        WHERE equivalent_distance_by_day.activity_type_id = 1 -- run
+            AND activity_equivalence_id = 3) -- loose
+    SELECT monday AS week_start
             , DATE_PART(''isodow'', activity_date) AS day
             , ROUND(CAST(distance_miles AS NUMERIC), 1) AS distance_miles
         FROM week_boundaries
-        LEFT JOIN equivalent_distance_per_day
+        LEFT JOIN equivalent_run_distance_by_day
             ON monday <= activity_date
             AND activity_date < next_monday
-        WHERE equivalent_distance_per_day.activity_type_id = 1 -- run
-            AND activity_equivalence_id = 3 -- loose
         ORDER BY week_start, day',
         'SELECT DISTINCT DATE_PART(''isodow'', date) AS dow FROM days ORDER BY dow')
     AS (week_start DATE, monday NUMERIC, tuesday NUMERIC, wednesday NUMERIC, thursday NUMERIC, friday NUMERIC, saturday NUMERIC, sunday NUMERIC)
