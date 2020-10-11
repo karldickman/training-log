902d853e23f4f20b9a32632dd69aed2e6092b912
karldickman <karldickman@gmail.com>
2020-10-11 13:14:43 -0700

    Route distance as fallback rather than overriding explicitly specified distance.

--- sql/views/activity_distances.sql
+++ sql/views/activity_distances.sql
@@ -1,15 +1,15 @@
 -- DROP VIEW activity_distances;
 CREATE OR REPLACE VIEW activity_distances
 AS
-WITH with_routes AS (SELECT activity_id, activity_date, distance_miles, activity_type_id
+WITH without_routes AS (SELECT activity_id, activity_date, distance_miles, activity_type_id
+    FROM activities
+    JOIN activity_non_route_distances USING (activity_id)),
+with_routes AS (SELECT activity_id, activity_date, distance_miles, activity_type_id
     FROM activities
     JOIN activity_routes USING (activity_id)
-    JOIN routes USING (route_id)),
-without_routes AS (SELECT activity_id, activity_date, distance_miles, activity_type_id
-    FROM activities
-    JOIN activity_non_route_distances USING (activity_id)
-    WHERE activities.activity_id NOT IN (SELECT with_routes.activity_id
-        FROM with_routes))
+    JOIN routes USING (route_id)
+    WHERE activity_id NOT IN (SELECT activity_id
+            FROM activity_non_route_distances))
 SELECT activity_id, activity_date, distance_miles, activity_type_id
     FROM with_routes
 UNION
