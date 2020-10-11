2b6d3c436bad4c29d16164ebc058e7520f7933e0
karldickman <karldickman@gmail.com>
2020-10-11 13:15:04 -0700

    JOIN USING instead of NATURAL JOIN

--- sql/views/equipment_distance.sql
+++ sql/views/equipment_distance.sql
@@ -1,8 +1,8 @@
-CREATE VIEW equipment_distance
+CREATE OR REPLACE VIEW equipment_distance
 AS
 SELECT equipment_id, SUM(distance_miles) AS distance_miles
     FROM activity_equipment
-    NATURAL JOIN activity_distances
-    GROUP By equipment_id;
+    JOIN activity_distances USING (activity_id)
+    GROUP BY equipment_id;
 
 ALTER VIEW equipment_distance OWNER TO postgres;
