daee0e64f407f690ee5312aaa2f0d8e564a13bd5
karldickman <karldickman@gmail.com>
2021-08-01 08:38:50 -0700

    Modify view that generates list of days to be more comprehensive.

--- sql/views/days.sql
+++ sql/views/days.sql
@@ -1,14 +1,18 @@
 -- DROP VIEW "days";
 CREATE OR REPLACE VIEW "days"
 AS
-WITH first_day AS (SELECT MIN(activity_date) AS first_day
-    FROM activities),
+WITH all_days AS (SELECT activity_date AS day
+    FROM activities
+UNION
+SELECT date_trunc('day', NOW())),
+first_days AS (SELECT MIN("day") AS first_day
+    FROM all_days),
 days_since_first_monday AS (SELECT first_day, MOD(CAST(DATE_PART('dow', first_day) - 1 + 7 AS INT), 7) AS days_since_monday
-    FROM first_day)
+    FROM first_days)
 SELECT date_trunc('day', dates)::date AS date
     FROM generate_series(
         (SELECT DATE(first_day - (days_since_monday || ' day')::INTERVAL) FROM days_since_first_monday),
-        (SELECT MAX(activity_date) FROM activities),
+        (SELECT MAX("day") FROM all_days),
         '1 day'::interval) AS dates;
 
 ALTER VIEW "days" OWNER TO postgres;
