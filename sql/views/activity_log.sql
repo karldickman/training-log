266d351440a585da6ccd36b848359191060cb3b3
karldickman <karldickman@gmail.com>
2021-08-01 08:36:23 -0700

    Modify activity log view to use rounded distance instead of string-formatted distance.

--- sql/views/activity_log.sql
+++ sql/views/activity_log.sql
@@ -6,12 +6,13 @@ SELECT activity_id
         , activity_description AS description
         , activity_type AS type
         , TO_CHAR((activity_durations.duration_minutes || ' minute')::INTERVAL, 'HH24:MI:SS') AS duration
-        , TO_CHAR(activity_distances.distance_miles, '999.9') AS distance
+        , ROUND(activity_distances.distance_miles::NUMERIC, 1) AS distance
         , CASE
             WHEN activity_type = 'bike'
                 THEN TO_CHAR(speed_miles_per_hour, '99.9') || ' mph'
             ELSE TO_CHAR((pace_minutes_per_mile || ' minute')::INTERVAL, 'MI:SS')
             END AS pace
+        , heart_rate_bpm AS heart_rate
         , equipment_label AS equipment
         , notes
         , url
@@ -22,9 +23,12 @@ SELECT activity_id
     LEFT JOIN activity_durations USING (activity_id)
     LEFT JOIN activity_distances USING (activity_id)
     LEFT JOIN activity_paces USING (activity_id)
+    LEFT JOIN activity_heart_rate USING (activity_id)
     LEFT JOIN activity_equipment USING (activity_id)
     LEFT JOIN equipment_labelled USING (equipment_id)
     LEFT JOIN activity_route_urls USING (activity_id)
+    WHERE activity_heart_rate_id IS NULL
+        OR summary_statistic_id = 1 -- Average
     ORDER BY activities.activity_date DESC, activities.activity_id DESC;
 
 ALTER TABLE activity_log OWNER TO postgres;
