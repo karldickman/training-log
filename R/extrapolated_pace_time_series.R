613076f464f351dcc42603ea101e52bd121c5611
Karl Dickman <karldickman@gmail.com>
2023-04-16 15:02:00 -0700

    Improved x and y axis scale steps.

--- R/extrapolated_pace_time_series.R
+++ R/extrapolated_pace_time_series.R
@@ -25,11 +25,14 @@ main <- function (argv) {
     paste(normalized.race.distance.km, "km"),
     paste(normalized.race.distance.km * 1000, "m"))
   # Plot data
-  workout.interval.splits() %>%
-    mutate(extrapolated_pace = lap_split_seconds + 5 * log(normalized.race.distance.km / race_distance_km) / log(2)) %>%
+  data <- workout.interval.splits() %>%
+    mutate(extrapolated_pace = lap_split_seconds + 5 * log(normalized.race.distance.km / race_distance_km) / log(2))
+  data %>%
     ggplot(aes(x = activity_date, y = extrapolated_pace, col = race_distance_km)) +
     geom_point() +
     geom_smooth() +
+    scale_x_date(date_breaks = "1 month", date_labels = "%m") +
+    scale_y_continuous(breaks = seq(floor(min(data$extrapolated_pace) / 5) * 5, ceiling(max(data$extrapolated_pace) / 5) * 5, 5)) +
     labs(
       title = paste("Interval lap paces standardized to", distance.label, "race pace"),
       subtitle = paste0("pace + 5 lb(", normalized.race.distance.km, " km / target race km)"),
