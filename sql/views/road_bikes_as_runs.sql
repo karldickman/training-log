2bd48c21fca25ca0a4103088621627ea75293477
Karl Dickman <karldickman@gmail.com>
2023-05-18 06:30:02 -0700

    Add biking to running mileage conversion when there is no recorded biking duration.

--- sql/views/road_bikes_as_runs.sql
+++ sql/views/road_bikes_as_runs.sql
@@ -9,6 +9,18 @@ SELECT activity_id
         , distance_miles * (0.0003736 * POWER(speed_miles_per_hour, 2) + 0.1973) AS run_distance_miles
         , activity_type_id
     FROM activity_paces
-    WHERE activity_type_id = 2; -- road bike
+    WHERE activity_type_id = 2 -- road bike
+UNION
+SELECT activity_id
+        , activity_date
+        , distance_miles AS bike_distance_miles
+        , NULL AS duration_minutes
+        , NULL AS speed_miles_per_hour
+        , distance_miles * 0.1973 AS run_distance_miles
+        , activity_type_id
+	FROM activity_distances
+    WHERE activity_type_id = 2 -- road bike
+    	AND activity_id NOT IN (SELECT activity_id 
+    		FROM activity_paces);
 
 ALTER VIEW road_bikes_as_runs OWNER TO postgres;
