c90afb0c9348128e6ba58b813a22c137374a1748
Karl Dickman <karldickman@gmail.com>
2023-05-21 09:05:37 -0700

    Write Python script to save workout intervals from a CSV file.

---
+++ save_intervals.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+"""Command-line tool to parse a CSV file of intervals and store that in the database."""
+
+from argparse import ArgumentParser, FileType
+from datetime import datetime
+from sys import exit, stdin
+from training_database import new_connection
+
+def parse_arguments():
+    parser = ArgumentParser(
+        prog = "workout-intervals",
+        description = "Reads a CSV file containing workout intervals and saves that to the database.")
+    parser.add_argument("activity_id", type = int, help = "The database ID of the activity whose intervals to record.")
+    #parser.add_argument("date", type = lambda d: datetime.strptime(d, "%Y-%m-%d").date(), help = "The date of the interval workout")
+    parser.add_argument("-f", "--file", type = FileType("r"), default = stdin, help = "The file containing the splits.  Defaults to STDIN.")
+    parser.add_argument("--preview", action = "store_true", help = "Show but do not execute database commands.")
+    return parser.parse_args()
+
+def main():
+    arguments = parse_arguments()
+    with new_connection(arguments.preview) as database, database.cursor() as cursor:
+        for interval, line in enumerate(arguments.file):
+            try:
+                row = line[0:-1].split(",")
+                distance_meters = float(row[0])
+                duration_datetime = datetime.strptime(row[1], "%M:%S.%f")
+                duration_minutes = duration_datetime.minute * 60 + duration_datetime.second + duration_datetime.microsecond / 1_000_000
+                target_split_seconds = float(row[2]) if len(row) > 2 else None
+                target_race_distance_meters = float(row[3]) if len(row) > 3 else None
+            except ValueError as e:
+                print("Syntax error in row", interval, row, str(e))
+                return 1
+            params = arguments.activity_id, interval + 1, distance_meters, duration_minutes, target_split_seconds, target_race_distance_meters
+            cursor.callproc("record_interval", params)
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
---
+++ sql/procedures/record_interval.sql
@@ -0,0 +1,49 @@
+CREATE OR REPLACE FUNCTION record_interval (
+    activity_id integer,
+    interval_number integer,
+    distance_meters double precision,
+    split_seconds double precision,
+    target_lap_split_seconds double precision DEFAULT NULL,
+    target_race_distance_km double precision DEFAULT NULL)
+RETURNS integer
+AS $$
+DECLARE activity_interval_id integer;
+begin
+	-- Database id
+    INSERT INTO activity_intervals
+        (activity_id, "interval")
+        VALUES
+        (activity_id, interval_number)
+        RETURNING activity_intervals.activity_interval_id
+        INTO activity_interval_id;
+    -- Split
+   	INSERT INTO activity_interval_total_splits
+   		(activity_interval_id, split_seconds)
+   		VALUES
+   		(activity_interval_id, split_seconds);
+   	-- Distance
+   	INSERT INTO activity_interval_distances
+   		(activity_interval_id, distance_meters)
+   		VALUES
+   		(activity_interval_id, distance_meters);
+   	-- Target lap split
+    IF target_lap_split_seconds IS NOT NULL THEN
+	   	INSERT INTO activity_interval_targets
+	   		(activity_interval_id, target_lap_split_seconds)
+	   		VALUES
+	   		(activity_interval_id, target_lap_split_seconds);
+	END IF;
+   	-- Target race distance
+    IF target_race_distance_km IS NOT NULL THEN
+	   	INSERT INTO activity_interval_target_race_distances
+	   		(activity_interval_id, race_distance_km)
+	   		VALUES
+	   		(activity_interval_id, target_race_distance_km);
+	END IF;
+   	-- All done!
+    RETURN(activity_interval_id);
+END;
+$$ LANGUAGE plpgsql
+SECURITY DEFINER SET search_path = public;
+
+ALTER FUNCTION record_interval OWNER TO postgres;
