6620321cd8a8d874dd380fdadf95033fc83ada65
Karl Dickman <karldickman@gmail.com>
2023-06-07 05:32:37 -0700

    Use zoo rollmean function to show trendline on the pace time series.

--- R/pace_time_series.R
+++ R/pace_time_series.R
@@ -1,13 +1,14 @@
 library(dplyr)
 library(ggplot2)
+library(zoo)
 
 source("database.R")
 
 plot <- function (data) {
   data %>%
     ggplot(aes(x = activity_date, y = pace_minutes_per_mile)) +
-    geom_point(size = 0.5, alpha = 0.8) +
-    geom_smooth() +
+    geom_point(size = 0.5, alpha = 0.5) +
+    geom_line(aes(y = rollmean(pace_minutes_per_mile, 14, na.pad = TRUE))) +
     scale_x_date(date_breaks = "1 years", date_labels = "%Y") +
     scale_y_continuous(breaks = c(6, 7, 8, 9, 10, 11, 12)) +
     labs(title = "Running mile pace time series") +
