2d903d5aea491d0b5c1a54e382aa4768a2f6a7c4
Karl Dickman <karldickman@gmail.com>
2023-06-07 05:25:55 -0700

    Add line annotations with dates of COVID and joining Rose City.

--- R/pace_heart_rate_time_series.R
+++ R/pace_heart_rate_time_series.R
@@ -33,6 +33,7 @@ main <- function (argv = c()) {
     geom_point(size = 0.5) +
     geom_line(aes(y = rollmean(pace_difference_from_trend, 14, na.pad = TRUE)), color = "#888888") +
     geom_hline(yintercept = 0) +
+    geom_vline(xintercept = as.numeric(as.Date(c("2021-07-24", "2022-07-03"))), linetype = 2) +
     scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m") +
     scale_y_continuous(breaks = y.axis.breaks) +
     labs(title = "Difference from heart rate–pace trend") +
