3325bf1c83172c5a5f9fe57aa392496ae41d258a
Karl Dickman <karldickman@gmail.com>
2023-11-12 09:29:26 -0800

    Replace zoo library with slider library for visualization of rolling average pace.

--- R/pace_time_series.R
+++ R/pace_time_series.R
@@ -1,14 +1,20 @@
 library(dplyr)
 library(ggplot2)
-library(zoo)
+library(slider)
 
 source("database.R")
 
-plot <- function (data) {
-  data %>%
+plot <- function (data, rolling.avg.window) {
+  data %>% mutate(
+    rolling_avg = slide_index_dbl(
+      pace_minutes_per_mile,
+      activity_date,
+      ~mean(.x, na.rm = TRUE),
+      .before = days(rolling.avg.window - 1))
+  ) %>%
     ggplot(aes(x = activity_date, y = pace_minutes_per_mile)) +
     geom_point(size = 0.5, alpha = 0.5) +
-    geom_line(aes(y = rollmean(pace_minutes_per_mile, 14, na.pad = TRUE))) +
+    geom_line(aes(y = rolling_avg), color = "#888888") +
     scale_x_date(date_breaks = "1 years", date_labels = "%Y") +
     scale_y_continuous(breaks = c(6, 7, 8, 9, 10, 11, 12)) +
     labs(title = "Running mile pace time series") +
@@ -16,7 +22,8 @@ plot <- function (data) {
     ylab("Pace (minutes/mile)")
 }
 
-main <- function (argv = c()) {
+main <- function (argv = c(14)) {
+  rolling.avg.window <- argv[[1]]
   # Load data
   pace.time.series <- using.database(function (fetch.query.results) {
     "SELECT *
@@ -29,5 +36,5 @@ main <- function (argv = c()) {
       fetch.query.results()
   })
   # Plot data
-  plot(pace.time.series)
+  plot(pace.time.series, rolling.avg.window)
 }
