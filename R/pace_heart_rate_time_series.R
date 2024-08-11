c778189d2ded21123b195be1e22fdc2100c54191
Karl Dickman <karldickman@gmail.com>
2024-08-11 08:14:16 -0700

    Add annotations for breaks and injury to pace/heart rate time series.

--- R/pace_heart_rate_time_series.R
+++ R/pace_heart_rate_time_series.R
@@ -31,18 +31,51 @@ add.trend.comparisons <- function (data, rolling.avg.window) {
     )
 }
 
-plot <- function (data, covid.infections, joined.rctc, step = 30) {
+trim.annotations.to.time.series <- function (annotations, date.min, date.max) {
+  starts <- c()
+  ends <- c()
+  for (i in 1:length(annotations[["start"]])) {
+    start <-annotations[["start"]][[i]]
+    end <- annotations[["end"]][[i]]
+    if (end < date.min | start > date.max) {
+      next
+    }
+    if (start < date.min & end > date.min) {
+      start <- date.min
+    }
+    if (start < date.max & end > date.max) {
+      end <- date.max
+    }
+    starts <- c(starts, start)
+    ends <- c(ends, end)
+  }
+  list(start = as.Date(starts), end = as.Date(ends))
+}
+
+annotate.breaks <- function (breaks, y.min, y.max) {
+  annotate("rect", xmin = breaks[["start"]], xmax = breaks[["end"]], ymin = y.min, ymax = y.max, alpha = 0.3)
+}
+
+annotate.injuries <- function (injuries, y.min, y.max) {
+  annotate("rect", xmin = injuries[["start"]], xmax = injuries[["end"]], ymin = y.min, ymax = y.max, alpha = 0.1, fill = "red")
+}
+
+plot <- function (data, covid.infections, joined.rctc, breaks, injuries, step = 30) {
   y.axis.breaks <- seq(
     floor(min(data$pace_difference_from_fit_seconds_per_mile) / step) * step,
     ceiling(max(data$pace_difference_from_fit_seconds_per_mile) / step) * step, step)
+  y.min <- min(y.axis.breaks)
+  y.max <- max(y.axis.breaks)
   ggplot(data, aes(x = activity_date, y = pace_difference_from_fit_seconds_per_mile)) +
+    annotate.injuries(injuries, y.min, y.max) +
+    annotate.breaks(breaks, y.min, y.max) +
     geom_point(size = 0.5) +
     geom_line(aes(y = rolling.avg), color = "#888888") +
     geom_hline(yintercept = 0) +
     geom_vline(xintercept = as.numeric(covid.infections), linetype = 2) +
     geom_vline(xintercept = as.numeric(joined.rctc), linetype = 3) +
-    scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m") +
-    scale_y_continuous(breaks = y.axis.breaks) +
+    scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m", expand = c(0.01, 0.01)) +
+    scale_y_continuous(breaks = y.axis.breaks, limits = c(y.min, y.max), expand = c(0, 0)) +
     labs(title = "Difference from heart rate–pace trend") +
     xlab("Run date") +
     ylab("Difference from Trend (s/mi)") +
@@ -121,9 +154,26 @@ main <- function (argv = c()) {
   args <- parse.args(argv)
   rolling.avg.window <- args$window
   from <- args$from
+  # Fetch data from database
+  data <- fetch.data(from)
+  # Dates of interest
   covid.infections <- as.Date(c("2021-07-24", "2024-02-19"))
   joined.rctc <- as.Date("2022-07-03")
-  fetch.data(from) |>
+  # Breaks and injuries
+  breaks <- list(
+    start = as.Date(c("2022-11-07", "2023-06-26", "2023-11-19", "2024-02-20", "2024-06-16")),
+    end = as.Date(c("2022-11-20", "2023-07-11", "2023-12-01", "2024-03-07", "2024-06-29"))
+  )
+  injuries <- list(
+    start = as.Date(c("2021-05-20", "2022-08-22", "2023-04-15")),
+    end = as.Date(c("2021-06-15", "2022-09-13", "2024-04-16"))
+  )
+  min.date <- min(data$activity_date)
+  max.date <- max(data$activity_date)
+  breaks <- trim.annotations.to.time.series(breaks, min.date, max.date)
+  injuries <- trim.annotations.to.time.series(injuries, min.date, max.date)
+  # Draw the plot
+  data |>
     add.trend.comparisons(rolling.avg.window) |>
-    plot(covid.infections, joined.rctc)
+    plot(covid.infections, joined.rctc, breaks, injuries)
 }
