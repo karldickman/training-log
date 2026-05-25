bf829fc2a4e08e69587fc45349e3fb9cfa951fdd
Karl Dickman <karldickman@gmail.com>
2026-05-24 22:48:07 -0700

    Bring back single chart annual mileage comparison option.

--- R/cumulative_miles_by_year.R
+++ R/cumulative_miles_by_year.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
+library(ggrepel)
 library(lubridate)
+library(viridis)
 
 source("data.R")
 source("lifetime_running_miles.R")
@@ -14,7 +16,29 @@ prepare_cumulative_miles <- function (data) {
     ungroup()
 }
 
-plot_cumulative_miles <- function (data) {
+goal <- tibble(
+  day = as.Date("2020-01-01") + 0:364,
+  cumulative_distance_mi = (1500 / 365) * (0:364)
+)
+
+plot_cumulative_miles_faceted <- function (data) {
+  data |>
+    ggplot(aes(x = day, y = cumulative_distance_mi)) +
+    geom_line() +
+    geom_line(data = goal, linetype = "dashed") +
+    facet_wrap(~ year) +
+    scale_x_date(date_breaks = "3 month", date_labels = "%b") +
+    labs(
+      title = "Cumulative miles per year",
+      x = "Day of year",
+      y = "Cumulative miles",
+    ) +
+    theme(legend.position = "bottom")
+}
+
+plot_cumulative_miles_combined <- function (data) {
+  data <- data |>
+    mutate(line_size = if_else(year == year(Sys.Date()), 1.5, 0.5))
   labels <- data |>
     group_by(year) |>
     filter(day == max(day)) |>
@@ -22,9 +46,20 @@ plot_cumulative_miles <- function (data) {
   data |>
     ggplot(aes(
       x = day,
-      y = cumulative_distance_mi
+      y = cumulative_distance_mi,
+      group = year,
+      color = year,
+      linewidth = line_size,
     )) +
     geom_line() +
+    geom_text_repel(
+      data = labels,
+      aes(label = year),
+      color = "black",
+      box.padding = 0.5,
+      point.padding = 0.5,
+      nudge_x = 5,
+    ) +
     geom_line(
       data = tibble(
         day = as.Date("2020-01-01") + 0:364,
@@ -32,23 +67,31 @@ plot_cumulative_miles <- function (data) {
       ),
       aes(x = day, y = cumulative_distance_mi),
       inherit.aes = FALSE,
-      linetype = "dashed"
+      linetype = "dashed",
     ) +
-    facet_wrap(~ year) +
-    scale_x_date(date_breaks = "3 month", date_labels = "%b") +
+    scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+    scale_color_viridis(option = "mako", begin = 0.8, end = 0) +
     scale_linewidth_identity() +
     labs(
       title = "Cumulative miles per year",
       x = "Day of year",
       y = "Cumulative miles",
-      color = "Year"
+      color = "Year",
     ) +
     theme(legend.position = "bottom")
 }
 
-main <- function (activity.equivalence = "loose") {
+main <- function (plot.type = "faceted", activity.equivalence = "loose") {
   data <- fetch_lifetime_running_miles() |>
     filter(activity_equivalence == activity.equivalence) |>
     prepare_cumulative_miles()
-  plot_cumulative_miles(data)
+  if (plot.type == "faceted") {
+    data |>
+      plot_cumulative_miles_faceted()
+  } else if (plot.type == "single") {
+    data |>
+      plot_cumulative_miles_combined()
+  } else {
+    stop(paste("Unknown plot type", plot.type))
+  }
 }
